b4a0ce5d197ab9c0988a818cd1724f53f0235602
PR-coderqueue <72966339+coderqueue-PR@users.noreply.github.com>
2020-11-08 12:00:20 +0530

    ReactJs Components Practice - 2

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Heading from './Heading';
+import List from './List';
+import Para from './Para';
+
+function App(){
+    return (
+        <>
+        <Heading/>
+        <Para/>
+        <List/>
+        <Heading/>
+        <Para/>
+        <List/>
+        </>
+    );
+}
+
+export default App;
